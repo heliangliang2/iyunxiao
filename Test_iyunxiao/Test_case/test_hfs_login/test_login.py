3065e47f02d3f776bb475a67a63316eaf0313828
test <246373146@qq.com>
2018-11-11 14:42:23 +0800

    111

--- Test_iyunxiao/Test_case/test_hfs_login/test_login.py
+++ Test_iyunxiao/Test_case/test_hfs_login/test_login.py
@@ -3,11 +3,12 @@ import sys,time
 from selenium import webdriver
 sys.path.append("C:\\Users\\Administrator\\Desktop\\iyunxiao\\iyunxiao\\Test_iyunxiao\\Test_case")
 from function.function import hfs_login
-class test_hfs_center_login(unittest.TestCase):
+class test_login(unittest.TestCase):
     '''测试账号登录'''
     def setUp(self):
         print("------开始执行账号登录测试------")
     def test_hfs_login(self):
+        '''正确的用户名和密码'''
         username="heliangliang"
         password="123456"
         hfs_login(username,password)
